a886d1128b114ba03249e7d7079d7702ae9d48ef
vanshshah31 <157968568+vanshshah31@users.noreply.github.com>
2024-10-28 20:03:32 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,1 @@
-import java.util.*;
 
-public class j28_10 {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-    }
-}
